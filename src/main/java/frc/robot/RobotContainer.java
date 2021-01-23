f8136fcb4161e8edcd6cc06ef5e30eb36bfd9e98
Joseph W. Carpenter <carp5@rcn.com>
2021-01-23 11:38:57 -0500

    Make Charge command time dependent

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -162,7 +162,7 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        Command charge = new ChargeAutoCommand(driveSubsystem, 0.2).withTimeout(1);
+        Command charge = new ChargeAutoCommand(driveSubsystem, 0.2, 1);
         return charge;
     }
 }
--- src/main/java/frc/robot/commands/ChargeAutoCommand.java
+++ src/main/java/frc/robot/commands/ChargeAutoCommand.java
@@ -1,14 +1,16 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
-public class ChargeAutoCommand extends CommandBase {
+public class ChargeAutoCommand extends WaitCommand {
 
     private final DriveSubsystem driveSubsystem;
     private final double speed;
 
-    public ChargeAutoCommand(DriveSubsystem drive, double speed) {
+    public ChargeAutoCommand(DriveSubsystem drive, double speed, double timeout) {
+        super(timeout);
         driveSubsystem = drive;
         this.speed = speed;
     }
