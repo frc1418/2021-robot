149e0f4f43b7751c780319c68c93ea6c3f14073f
Joseph W. Carpenter <carp5@rcn.com>
2021-02-12 14:47:40 -0500

    Make changes to DriveSubsystemTest

--- src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
+++ src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
@@ -2,7 +2,10 @@ package frc.robot.subsystems;
 
 import static org.mockito.Mockito.verify;
 
+import frc.robot.common.Odometry;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -12,10 +15,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class DriveSubsystemTest {
 
     @Mock DifferentialDrive driveTrain;
+    @Mock SpeedControllerGroup leftMotors;
+    @Mock SpeedControllerGroup rightMotors;
+    @Mock Odometry odometry;
 
     @Test
     public void testArcadeDrive() {
-        DriveSubsystem driveSubsystem = new DriveSubsystem(driveTrain);
+        DriveSubsystem driveSubsystem = new DriveSubsystem(driveTrain, leftMotors, rightMotors, odometry);
         driveSubsystem.drive(1, 0);
         verify(driveTrain).arcadeDrive(1, 0);
     }
