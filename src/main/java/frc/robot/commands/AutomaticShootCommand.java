e297b732fa2999aec8365d16d1ae8d807666649f
lamv13 <73197146+lamv13@users.noreply.github.com>
2021-02-20 15:21:26 -0500

    Updated formatting in AutomaticShootCommand

--- src/main/java/frc/robot/commands/AutomaticShootCommand.java
+++ src/main/java/frc/robot/commands/AutomaticShootCommand.java
@@ -1,32 +1,44 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutomaticShootCommand extends CommandBase {
     private final ShooterSubsystem shooterSubsystem;
+    private final Trigger ballSensorTrigger;
     private final double targetVel;
-    private final int ballNum;
+    private int numBalls;
     private int totalShoot = 0;
 
-    public AutomaticShootCommand(double targetVel, int ballNum, ShooterSubsystem shooterSubsystem) {
+    public AutomaticShootCommand(double targetVel, int numBalls, ShooterSubsystem shooterSubsystem) {
         this.targetVel = targetVel;
-        this.ballNum = ballNum;
+        this.numBalls = numBalls;
         this.shooterSubsystem = shooterSubsystem;
         addRequirements(shooterSubsystem);
+        ballSensorTrigger = new Trigger(this.shooterSubsystem::isBallReady);
+        ballSensorTrigger.whenActive(
+                new InstantCommand(
+                        () -> {
+                            totalShoot++;
+                            // logger.info("Total Balls Shot: " + totalShoot);
+                        }));
+    }
+
+    public void end(boolean interrupted) {
+        shooterSubsystem.shootVoltage(0);
     }
 
     public boolean isFinished() {
-        return totalShoot >= ballNum;
+        return (totalShoot >= numBalls);
     }
 
     public void execute() {
         shooterSubsystem.shootVelocity(targetVel);
-        if (shooterSubsystem.checkTarget() && shooterSubsystem.isBallReady()) {
+        if (shooterSubsystem.isAtTargetSpeed() && shooterSubsystem.isBallReady()) {
             shooterSubsystem.activatePiston();
-            totalShoot++;
-        }
-        else if (shooterSubsystem.checkTarget() && !shooterSubsystem.isBallReady())
+        } else if (shooterSubsystem.isAtTargetSpeed() && !shooterSubsystem.isBallReady())
             shooterSubsystem.lowerPiston();
     }
 }
