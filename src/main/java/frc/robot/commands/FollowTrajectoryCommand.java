5a7e994ff52f0cb30d5be938a4bc8c0df2bf625c
AndrewLester <alester220@gmail.com>
2021-02-21 11:44:16 -0500

    Fix formatting

--- src/main/java/frc/robot/commands/FollowTrajectoryCommand.java
+++ src/main/java/frc/robot/commands/FollowTrajectoryCommand.java
@@ -14,29 +14,32 @@ import frc.robot.subsystems.DriveSubsystem;
 
 public class FollowTrajectoryCommand extends SequentialCommandGroup {
 
-
-    public FollowTrajectoryCommand(Trajectory trajectory, Odometry odometry, DriveSubsystem driveSubsystem) {
+    public FollowTrajectoryCommand(
+            Trajectory trajectory, Odometry odometry, DriveSubsystem driveSubsystem) {
         this(trajectory, odometry, driveSubsystem, true);
     }
 
-    public FollowTrajectoryCommand(Trajectory trajectory, Odometry odometry, DriveSubsystem driveSubsystem, boolean resetOdometry) {
+    public FollowTrajectoryCommand(
+            Trajectory trajectory,
+            Odometry odometry,
+            DriveSubsystem driveSubsystem,
+            boolean resetOdometry) {
         if (resetOdometry) {
             odometry.reset(trajectory.getInitialPose());
         }
-        
+
         addCommands(
-            new RamseteCommand(
-                trajectory,
-                odometry::getPose,
-                new RamseteController(),
-                new SimpleMotorFeedforward(DRIVE_KS, DRIVE_KV, DRIVE_KA),
-                DriveSubsystem.KINEMATICS,
-                odometry::getWheelSpeeds,
-                new PIDController(0.06, 0, 0),
-                new PIDController(0.06, 0, 0),
-                driveSubsystem::tankDriveVolts,
-                driveSubsystem),
-            new InstantCommand(() -> driveSubsystem.tankDriveVolts(0, 0))
-        );
+                new RamseteCommand(
+                        trajectory,
+                        odometry::getPose,
+                        new RamseteController(),
+                        new SimpleMotorFeedforward(DRIVE_KS, DRIVE_KV, DRIVE_KA),
+                        DriveSubsystem.KINEMATICS,
+                        odometry::getWheelSpeeds,
+                        new PIDController(0.06, 0, 0),
+                        new PIDController(0.06, 0, 0),
+                        driveSubsystem::tankDriveVolts,
+                        driveSubsystem),
+                new InstantCommand(() -> driveSubsystem.tankDriveVolts(0, 0)));
     }
 }
