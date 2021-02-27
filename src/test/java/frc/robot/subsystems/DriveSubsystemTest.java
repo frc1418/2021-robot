6594d0c04338f884101e00d48391fb8cc09c3393
Joseph W. Carpenter <carp5@rcn.com>
2021-02-27 14:44:06 -0500

    Fix conflicts

--- src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
+++ src/test/java/frc/robot/subsystems/DriveSubsystemTest.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems;
 import static org.mockito.Mockito.verify;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.common.Odometry;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,11 +19,13 @@ public class DriveSubsystemTest {
     @Mock SpeedControllerGroup leftMotors;
     @Mock SpeedControllerGroup rightMotors;
     @Mock Odometry odometry;
+    @Mock Field2d field;
+    @Mock Timer timer;
 
     @Test
     public void testArcadeDrive() {
         DriveSubsystem driveSubsystem =
-                new DriveSubsystem(driveTrain, leftMotors, rightMotors, odometry);
+                new DriveSubsystem(driveTrain, leftMotors, rightMotors, odometry, field, timer);
         driveSubsystem.drive(1, 0);
         verify(driveTrain).arcadeDrive(1, 0, false);
     }
@@ -29,7 +33,7 @@ public class DriveSubsystemTest {
     @Test
     public void testJoystickDrive() {
         DriveSubsystem driveSubsystem =
-                new DriveSubsystem(driveTrain, leftMotors, rightMotors, odometry);
+                new DriveSubsystem(driveTrain, leftMotors, rightMotors, odometry, field, timer);
         driveSubsystem.joystickDrive(1, 0);
         verify(driveTrain).arcadeDrive(1, 0);
     }
